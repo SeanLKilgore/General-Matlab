17f6aa6a5e7901e631271c95dd6093dce86f5fe1
SeanLKilgore <141957294+SeanLKilgore@users.noreply.github.com>
2024-11-05 21:22:34 -0700

    Create example.m

---
+++ example.m
@@ -0,0 +1,12 @@
+classdef example < handle & matlab.mixin.CustomDisplay & matlab.mixin.CustomCompactDisplay
+  properties
+  end
+  methods % Constructor
+    function obj = example( varargin )
+    end
+  end
+  methods % Destructor
+    function delete(obj)
+    end
+  end
+end
