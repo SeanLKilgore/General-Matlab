57c41f5fc0ff6eed31e16f8b04d2141c45c8c6ac
seank <seank@192.168.0.45>
2025-02-24 19:55:35 -0700

    @TrackedObject/TriggerOnCreation() - call to ClassToName method instead of inbuilt regular expression

--- @TrackedObject/TriggerOnCreation.m
+++ @TrackedObject/TriggerOnCreation.m
@@ -16,11 +16,13 @@ function varargout = TriggerOnCreation( ~ , evt )
 % Get the metaclass of the (possibly) subclassed object
 mc = metaclass( evt.Instance );
 
-Name = regexprep( mc.Name , '[^a-zA-z0-9]+','_');
+% Name = regexprep( mc.Name , '[^a-zA-z0-9]+','_');
+Name = TrackedObject.ClassToName(mc.Name);
 
 TrackedObjectList = TrackedObject.TrackedObjectList;
 
 if ~isprop( TrackedObjectList , Name)
+    TrackedObjectList.(Name) = HandledObject;
     % Create a new property
     TrackedObjectList.(Name).Objects   = evt.Instance;
     evt.Instance.ObjectID = 0;
@@ -31,4 +33,5 @@ else
     evt.Instance.ObjectID = 1+max(TrackedObjectList.(Name).ObjectIDs);
     TrackedObjectList.(Name).ObjectIDs = [ TrackedObjectList.(Name).ObjectIDs evt.Instance.ObjectID ];
 end
+
 end
